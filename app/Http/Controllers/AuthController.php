a1582b5b792b9fc25b3dd7cd6dd652c77a3dd237
sarfaraz-dev <muhammadsarfraz531@gmail.com>
2022-02-16 14:12:44 +0500

    auth api with middelware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,14 +10,20 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    // test api/middleware function starts
     public function test()
     {
-        return response()->json(['success' => true, 'message' => 'suasdadasdasdasdccess']);
-    }
+        $message = 'success';
+        $user = 'Test User';
+        return (new ResponseController)->sendResponse(1, $message, $user);
 
+    }
+    // test api/middleware  function ends
+
+    // register api for user starts
     public function register(Request $request)
     {
-        // return($request->all());
+        // validate inputs
         $validator = Validator::make($request->all(), [
             'f_name' => 'required|string',
             'l_name' => 'required|string',
@@ -33,10 +39,12 @@ class AuthController extends Controller
             'town' => 'required',
         ]);
 
+        // if validator fails
         if ($validator->fails()) {
             return (new ResponseController)->sendError(0, $validator->errors());
         }
 
+        // password encryption
         $request['password'] = Hash::make($request['password']);
         // if ($request['role_id'] == 2) {
 
@@ -51,38 +59,37 @@ class AuthController extends Controller
         // $request['phone'] = strip_tags($request['phone']);
         // $request['phone'] = substr($request['phone'], -10);
 
+        // // save profile picture of user
         // if ($request->has('profile_pic')) {
-
         //     $file = $request->file('profile_pic');
-
         //     $name = $file->getClientOriginalName();
         //     $name = date("dmyHis.") . gettimeofday()["usec"] . '_' . $name;
-
         //     $directory = "assets/backend/image/user_profile_pic";
         //     //$path = base_path() . "/public" . $directory;
         //     $path = $directory;
-
         //     $file->move($path, $name);
-
         //     $request['picture'] = $path . '/' . $name;
         // }
+
+        // create new user and save db
         $user = User::create($request->except(["confirm_password"]));
         if ($user) {
 
-            // return   $user['token'] ;
+            // check verification of user
             if ($user->is_verified == 1) {
                 $message = "Registration successful";
             } else {
                 $message = "Your account is under verification!";
             }
+            // get registered user to return token
             $user = User::find($user->id);
             $user['token'] = $user->createToken('token')->accessToken;
 
             // if ($user->picture != null) {
-
             //     $user->picture = getImageUrl($user->picture);
             // }
 
+            // return response
             return (new ResponseController)->sendResponse(1, $message, $user);
         } else {
             $error = "Something went wrong! Please try again";
@@ -90,18 +97,18 @@ class AuthController extends Controller
         }
 
     }
+    // register api for user function ends
 
-    //login APi for user sign in
+    //login APi for user to sign in function starts
     public function login(Request $request)
     {
-
+        // validate the email,user name and password
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'password' => 'required',
         ]);
 
         // $platform = $request['signup_source'];
-
         /*if(strtolower($platform) == 'ios'){
         $validator = Validator::make($request->all(), [
         'app_version' => 'required',
@@ -112,22 +119,27 @@ class AuthController extends Controller
         ]);
         }*/
 
+        // if validator fails
         if ($validator->fails()) {
-
             return (new ResponseController)->sendError(0, $validator->errors());
         }
 
         //$status =  (new ResponseController)->check_version($request->all());
-
         // $request['phone'] = strip_tags($request['phone']);
         // $request['phone'] = substr($request['phone'], -10);
         // return $request->name;
+
+        // if user is logging in through email
         if (filter_var($request['name'], FILTER_VALIDATE_EMAIL)) {
+            // if credentials are not correct
             if (!Auth::attempt(['email' => $request->name, 'password' => $request->password])) {
                 $error = "Phone or password does not match!";
                 return (new ResponseController)->sendError(0, $error);
             }
-        } else {
+        }
+        // if user is logging in through username
+        else {
+            // if credentials are not correct
             if (!Auth::attempt(['name' => $request->name, 'password' => $request->password])) {
                 $error = "Phone or password does not match!";
                 return (new ResponseController)->sendError(0, $error);
@@ -140,23 +152,19 @@ class AuthController extends Controller
         // }
         $user = $request->user();
         $user['token'] = $user->createToken('token')->accessToken;
-
-        //UserDevice::updateOrCreate(["serial" => $request['serial']],array_merge($request->except(['name', 'phone', 'password', 'login_with']),['status' => 'A','user_id' => $user->id]));
-
+        // check if user is logged in successfully
         if ($user) {
-            // dd($user);
+            // if user is not verified yet
             if ($user->is_verified == 0) {
-
                 Auth::logout();
-
                 $status = -1;
                 $message = "Sorry! Your account is not verified by admin.";
                 return (new ResponseController)->sendError($status, $message);
 
             } else {
 
+                // // show user image
                 // if($user->picture != null) {
-
                 //     $user->picture = getImageUrl($user->picture);
                 // }
 
@@ -170,4 +178,6 @@ class AuthController extends Controller
             return (new ResponseController)->sendError(0, $message);
         }
     }
+    //login APi for user to sign in function ends
+
 }
