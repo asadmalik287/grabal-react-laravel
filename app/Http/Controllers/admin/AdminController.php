0b386960f91983712fa060b66a9005f72f7fbfb6
sarfaraz-dev <muhammadsarfraz531@gmail.com>
2022-02-18 15:34:47 +0500

    login handle

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
     public function logout()
     {
         FacadesAuth::logout();
-        return redirect()->route('adminLogout');
+        return redirect()->route('login');
 
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,3 @@
-{{-- <form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit">Logout</button>
-  </form> --}}
-{{--
-<a href="{{ url('admin/logout') }}"> logout</a> --}}
+
+<a href="{{ url('admin/logout') }}"> logout</a> 
     Hello admin panel!
