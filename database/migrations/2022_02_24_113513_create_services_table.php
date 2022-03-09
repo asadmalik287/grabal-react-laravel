9db36a146d4d9fd697e5227c1e8c1f08f1542a49
zohaib531 <82142921+zohaib531@users.noreply.github.com>
2022-03-09 19:12:39 +0500

    changes

--- database/migrations/2022_02_24_113513_create_services_table.php
+++ database/migrations/2022_02_24_113513_create_services_table.php
@@ -14,7 +14,7 @@ class CreateServicesTable extends Migration
     public function up()
     {
         Schema::create('services', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('title');
             $table->string('description')->nullable();
             $table->text('contact_name')->nullable();
