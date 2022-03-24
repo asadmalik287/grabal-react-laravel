3b0c72d4703af402e6ec3a02d47373058e694797
zohaib531 <82142921+zohaib531@users.noreply.github.com>
2022-03-24 12:51:15 +0500

    chanegs

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -107,7 +107,7 @@
                                     </thead>
                                     <tbody>
                                         @forelse ($allServices as $key => $allService)
-                                        @dd($allService->hasCategory)
+                                        @dd($allService->hasCategory->name)
                                             <tr>
                                                 <td>{{ $key + 1 }}</td>
                                                 <td class="wsnw">{{ $allService->title }}</td>
