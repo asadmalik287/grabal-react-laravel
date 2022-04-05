ecee5541ba59d3b1a7df2580e5a7ed639303b71f
zohaib531 <82142921+zohaib531@users.noreply.github.com>
2022-04-05 12:34:28 +0500

    checking subscription

--- app/Http/Controllers/api/ServiceController.php
+++ app/Http/Controllers/api/ServiceController.php
@@ -354,7 +354,7 @@ class ServiceController extends Controller
     {
         if (isset($_GET['id'])) {
             $seller = User::where('id', $_GET['id'])->first();
-            $subscription = Subscription::where('id', $_GET['id'])->where('stripe_subscription_status', 'active')->first();
+            $subscription = Subscription::where('stripe_subscription_status', 'active')->where('id', $_GET['id'])->first();
             return $subscription;
             return response()->json(['seller' => $seller]);
         }
