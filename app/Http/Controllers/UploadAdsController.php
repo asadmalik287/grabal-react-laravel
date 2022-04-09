ac10aa41fc81eb5302bb50673eda1edc2994fe20
zohaib531 <82142921+zohaib531@users.noreply.github.com>
2022-04-09 16:09:48 +0500

    changes

--- app/Http/Controllers/UploadAdsController.php
+++ app/Http/Controllers/UploadAdsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\UploadAds;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\validator;
+use Validator;
 use Storage;
 
 class UploadAdsController extends Controller
