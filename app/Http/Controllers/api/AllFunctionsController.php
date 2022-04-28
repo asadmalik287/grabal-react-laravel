e34cc943dce7bacb0e0e506da92a844499c13d1e
zohaib531 <82142921+zohaib531@users.noreply.github.com>
2022-04-28 14:40:43 +0500

    getting slug in popular services

--- app/Http/Controllers/api/AllFunctionsController.php
+++ app/Http/Controllers/api/AllFunctionsController.php
@@ -170,7 +170,7 @@ class AllFunctionsController extends Controller
         $topTenServices = array_slice(array_keys($serviceArray), 0, 10);
         $topThreeServices = array_slice(array_keys($serviceArray), 0, $request->limit);
         // get top three services category and their service providers
-        $topServicesCategory = Service::select(['id', 'title', 'subCategory_id', 'added_by'])->whereIn('id', $topThreeServices)
+        $topServicesCategory = Service::select(['id', 'title','slug', 'subCategory_id', 'added_by'])->whereIn('id', $topThreeServices)
             ->with(['subcat' => function ($subCategory) {
                 $subCategory->select(['id', 'name', 'slug'])
                     ->where('status', 'active')
