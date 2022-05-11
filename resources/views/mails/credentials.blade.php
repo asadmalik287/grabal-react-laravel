fa8d3fae3c15e298d618d25647eb0b9b96a08c81
zohaib531 <82142921+zohaib531@users.noreply.github.com>
2022-05-11 11:45:48 +0500

    fixes and changes

--- resources/views/mails/credentials.blade.php
+++ resources/views/mails/credentials.blade.php
@@ -1,2 +1,18 @@
-name: {{ $userName }} <br>
-password: {{ $password }}
+
+<div style="text-align:center;display:flex;justify-content:center">
+    <img src="https://grobal.netlify.app/static/media/Logo.fe603d743e3ea8419ada.png" alt="">
+</div>
+<h5>Hello {{ $userName }},</h4> <br>
+<p>Thank you for creating your account.</p> <br>
+<p> We look forward to reading your posts and hope you will enjoy the space that we created for our customers.</p>
+<br>
+<br>
+<div><h4>Username: {{ $userName }}</h4></div>
+<div><h4>Password: {{ $password }}</h4></div>
+
+<p>Please use this username and password for login to you account</p> <br>
+
+
+
+
+
